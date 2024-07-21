ceb458732317ef0874573cba33df5783d880de88
alpardfm <tokferdian100@gmail.com>
2024-07-21 15:46:40 +0700

    add struct location

---
+++ src/entity/location.go
@@ -0,0 +1,18 @@
+package entity
+
+import "time"
+
+type Location struct {
+	ID        int64     `json:"id" db:"id,omitempty" param:"id"`
+	Lat       string    `json:"lat" db:"lat,omitempty" param:"lat"`
+	Long      string    `json:"long" db:"long,omitempty" param:"long"`
+	Distance  int64     `json:"distance" db:"distance,omitempty" param:"distance"`
+	Secret    string    `json:"secret" db:"secret,omitempty" param:"secret"`
+	IsDeleted int64     `db:"is_deleted" json:"is_deleted,omitempty" param:"is_deleted"`
+	CreatedAt time.Time `db:"created_at" json:"created_at,omitempty" param:"created_at"`
+	CreatedBy string    `db:"created_by" json:"created_by,omitempty" param:"created_by"`
+	UpdatedAt time.Time `db:"updated_at" json:"updated_at,omitempty" param:"updated_at"`
+	UpdatedBy string    `db:"updated_by" json:"updated_by,omitempty" param:"updated_by"`
+	DeletedAt time.Time `db:"deleted_at" json:"deleted_at,omitempty" param:"deleted_at"`
+	DeletedBy string    `db:"deleted_by" json:"deleted_by,omitempty" param:"deleted_by"`
+}
