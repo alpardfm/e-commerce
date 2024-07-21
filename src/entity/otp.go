a50f59be67d12024781d737c4d4128715387eeda
alpardfm <tokferdian100@gmail.com>
2024-07-21 15:47:07 +0700

    add struct otp

---
+++ src/entity/otp.go
@@ -0,0 +1,16 @@
+package entity
+
+import "time"
+
+type OTP struct {
+	ID        int64     `json:"id" db:"id,omitempty" param:"id"`
+	UserID    int64     `json:"user_id" db:"user_id,omitempty" param:"user_id"`
+	Code      string    `json:"code" db:"code,omitempty" param:"omitempty"`
+	IsDeleted int64     `db:"is_deleted" json:"is_deleted,omitempty" param:"is_deleted"`
+	CreatedAt time.Time `db:"created_at" json:"created_at,omitempty" param:"created_at"`
+	CreatedBy string    `db:"created_by" json:"created_by,omitempty" param:"created_by"`
+	UpdatedAt time.Time `db:"updated_at" json:"updated_at,omitempty" param:"updated_at"`
+	UpdatedBy string    `db:"updated_by" json:"updated_by,omitempty" param:"updated_by"`
+	DeletedAt time.Time `db:"deleted_at" json:"deleted_at,omitempty" param:"deleted_at"`
+	DeletedBy string    `db:"deleted_by" json:"deleted_by,omitempty" param:"deleted_by"`
+}
