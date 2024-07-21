11b151e8e3309f4b088c2e4def25ce758e30f670
alpardfm <tokferdian100@gmail.com>
2024-07-21 15:47:39 +0700

    add struct role

---
+++ src/entity/role.go
@@ -0,0 +1,15 @@
+package entity
+
+import "time"
+
+type Role struct {
+	ID        int64     `json:"id" db:"id,omitempty" param:"id"`
+	Name      string    `json:"name" db:"name,omitempty" param:"name"`
+	IsDeleted int64     `db:"is_deleted" json:"is_deleted,omitempty" param:"is_deleted"`
+	CreatedAt time.Time `db:"created_at" json:"created_at,omitempty" param:"created_at"`
+	CreatedBy string    `db:"created_by" json:"created_by,omitempty" param:"created_by"`
+	UpdatedAt time.Time `db:"updated_at" json:"updated_at,omitempty" param:"updated_at"`
+	UpdatedBy string    `db:"updated_by" json:"updated_by,omitempty" param:"updated_by"`
+	DeletedAt time.Time `db:"deleted_at" json:"deleted_at,omitempty" param:"deleted_at"`
+	DeletedBy string    `db:"deleted_by" json:"deleted_by,omitempty" param:"deleted_by"`
+}
