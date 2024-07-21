3391ae204aa291d0c3197a182517d339e377abfe
alpardfm <tokferdian100@gmail.com>
2024-07-21 15:46:48 +0700

    add struct order_items

---
+++ src/entity/order_items.go
@@ -0,0 +1,18 @@
+package entity
+
+import "time"
+
+type OrderItems struct {
+	ID        int64     `json:"id" db:"id,omitempty" param:"id"`
+	OrderID   int64     `json:"order_id" db:"order_id,omitempty" param:"order_id"`
+	ProductID int64     `json:"product_id" db:"product_id,omitempty" param:"product_id"`
+	Quantity  int64     `json:"quantity" db:"quantity,omitempty" param:"quantity"`
+	Price     float64   `json:"price" db:"price,omitempty" param:"price"`
+	IsDeleted int64     `db:"is_deleted" json:"is_deleted,omitempty" param:"is_deleted"`
+	CreatedAt time.Time `db:"created_at" json:"created_at,omitempty" param:"created_at"`
+	CreatedBy string    `db:"created_by" json:"created_by,omitempty" param:"created_by"`
+	UpdatedAt time.Time `db:"updated_at" json:"updated_at,omitempty" param:"updated_at"`
+	UpdatedBy string    `db:"updated_by" json:"updated_by,omitempty" param:"updated_by"`
+	DeletedAt time.Time `db:"deleted_at" json:"deleted_at,omitempty" param:"deleted_at"`
+	DeletedBy string    `db:"deleted_by" json:"deleted_by,omitempty" param:"deleted_by"`
+}
