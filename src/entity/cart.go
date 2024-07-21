a81e8eb2b62b70d1db045d152c7464988a9d773d
alpardfm <tokferdian100@gmail.com>
2024-07-21 15:46:27 +0700

    add struct cart

---
+++ src/entity/cart.go
@@ -0,0 +1,17 @@
+package entity
+
+import "time"
+
+type Cart struct {
+	ID        int64     `json:"id" db:"id,omitempty" param:"id"`
+	UserID    int64     `json:"user_id" db:"user,omitempty" param:"user_id"`
+	ProductID int64     `json:"product_id" db:"product_id,omitempty" param:"product_id"`
+	Quantity  int64     `json:"quantity" db:"quantity,omitempty" param:"quantity"`
+	IsDeleted int64     `db:"is_deleted" json:"is_deleted,omitempty" param:"is_deleted"`
+	CreatedAt time.Time `db:"created_at" json:"created_at,omitempty" param:"created_at"`
+	CreatedBy string    `db:"created_by" json:"created_by,omitempty" param:"created_by"`
+	UpdatedAt time.Time `db:"updated_at" json:"updated_at,omitempty" param:"updated_at"`
+	UpdatedBy string    `db:"updated_by" json:"updated_by,omitempty" param:"updated_by"`
+	DeletedAt time.Time `db:"deleted_at" json:"deleted_at,omitempty" param:"deleted_at"`
+	DeletedBy string    `db:"deleted_by" json:"deleted_by,omitempty" param:"deleted_by"`
+}
